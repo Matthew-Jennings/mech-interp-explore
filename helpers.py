53e6037000be0c27f1ff397487d813a2013b86b8
Matthew Jennings <matthew.jennings@outlook.com.au>
2024-04-16 17:03:53 +0930

    add print functionality to cumul_probs helper. extract patterns

--- helpers.py
+++ helpers.py
@@ -4,9 +4,18 @@ from jaxtyping import Float
 import torch as t
 from transformer_lens import HookedTransformer
 
+PATTERNS = {
+    "Space, First Char Uppercase": re.compile(r"\s[A-Z]\w*"),
+    "Space, First Char Lowercase": re.compile(r"\s[a-z]\w*"),
+    "Space, First Char Numeral": re.compile(r"\s[0-9]\w*"),
+    "No Space, First Char Uppercase": re.compile(r"[A-Z]\w*"),
+    "No Space, First Char Lowercase": re.compile(r"[a-z]\w*"),
+    "No Space, First Char Numeral": re.compile(r"[0-9]\w*"),
+}
+
 
 def cumul_probs_by_capitalisation_type(
-    logits: Float[t.Tensor, "d_vocab"], model: HookedTransformer, batch=0
+    logits: Float[t.Tensor, "d_vocab"], model: HookedTransformer, batch=0, print_=False
 ) -> dict:
     """
     Calculate the cumulative probability of predicting a token of each
@@ -23,27 +32,25 @@ def cumul_probs_by_capitalisation_type(
     if batch >= logits.size()[0]:
         raise ValueError("Bad batch index.")
 
-    patterns = {
-        "Space, Upper": re.compile(r"\s[A-Z]\w*"),
-        "Space, Lower": re.compile(r"\s[a-z]\w*"),
-        "Space, Numeral": re.compile(r"\s[0-9]\w*"),
-        "No Space, Upper": re.compile(r"[A-Z]\w*"),
-        "No Space, Lower": re.compile(r"[a-z]\w*"),
-        "No Space, Numeral": re.compile(r"[0-9]\w*"),
-    }
     probs = logits[batch].softmax(-1).cpu().numpy()
     str_tokens = model.to_str_tokens(t.arange(model.cfg.d_vocab))
 
-    cumul_probs = dict.fromkeys(patterns, 0)
+    cumul_probs = dict.fromkeys(PATTERNS, 0)
     cumul_probs["Other"] = 0
 
     for i, str_token in enumerate(str_tokens):
         matched = False
-        for pattern, regex in patterns.items():
-            if regex.match(str_token):
-                cumul_probs[pattern] += probs[i]
+        for pattern_name, pattern in PATTERNS.items():
+            if pattern.match(str_token):
+                cumul_probs[pattern_name] += probs[i]
                 matched = True
         if not matched:
             cumul_probs["Other"] += probs[i]
+
+    if print_:
+        for pattern, cumul_prob in sorted(
+            cumul_probs.items(), key=lambda x: x[1], reverse=True
+        ):
+            print(f"{pattern}: {100*cumul_prob:.2f}%")
 
     return cumul_probs
